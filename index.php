b339fa1dc4448875a34750777d60dcf1b859d157
Narendran Srinivasan <Narendran.Srinivasan@qfor.com>
2014-10-01 16:28:22 +0530

    Installed libraries

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width; initial-scale=1"/>
+        <link rel="stylesheet" href="bootstrap-3.2.0-dist/css/bootstrap.min.css" type="text/css"/>
+        <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="js/angular.min.js"></script>
+        <script type="text/javascript" src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <h1>FutureGo</h1>
+        </div>
+    </body>
+</html>
